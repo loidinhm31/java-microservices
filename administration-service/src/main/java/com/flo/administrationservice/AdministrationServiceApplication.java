5d9ffc57a2936aea541e1c1c7bb768017b388370
loidinhm31 <loidinh.git@gmail.com>
2022-12-09 01:08:37 +0700

    refactor: change authentication service to administration service which will be used to manage all other services

--- administration-service/src/main/java/com/flo/administrationservice/AdministrationServiceApplication.java
+++ administration-service/src/main/java/com/flo/administrationservice/AdministrationServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class AuthenticationServiceApplication {
+public class AdministrationServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthenticationServiceApplication.class, args);
+		SpringApplication.run(AdministrationServiceApplication.class, args);
 	}
 
 }
