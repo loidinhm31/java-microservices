57cd41363c15b090d7fe3d0ff0b3151da5f29c06
loidinhm31 <loidinh.git@gmail.com>
2022-12-09 00:44:59 +0700

    authenticate: use opaque token to introspect from oidc

--- authentication-service/src/main/java/com/flo/authenticationservice/security/SecurityConfig.java
+++ authentication-service/src/main/java/com/flo/authenticationservice/security/SecurityConfig.java
@@ -1,69 +1,40 @@
 package com.flo.authenticationservice.security;
 
-import com.flo.authenticationservice.security.jwt.JwtAuthFilter;
-import com.flo.authenticationservice.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final UserService userService;
+    @Value("${spring.security.oauth2.resourceserver.opaque.introspection-uri}")
+    String introspectionUri;
 
-    private final JwtAuthFilter jwtAuthFilter;
+    @Value("${spring.security.oauth2.resourceserver.opaque.introspection-client-id}")
+    String clientId;
 
-    public SecurityConfig(UserService userService, JwtAuthFilter jwtAuthFilter) {
-        this.userService = userService;
-        this.jwtAuthFilter = jwtAuthFilter;
-    }
+    @Value("${spring.security.oauth2.resourceserver.opaque.introspection-client-secret}")
+    String clientSecret;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
 
         http
-                .authorizeHttpRequests((authorize) ->
-                        authorize.requestMatchers("/*/auth/**").permitAll())
                 .authorizeHttpRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // stateless for jwt
-                .and()
-                .authenticationProvider(authenticationProvider())
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .opaqueToken(token -> token
+                                .introspectionUri(this.introspectionUri)
+                                .introspectionClientCredentials(this.clientId, this.clientSecret)))
         ;
         return http.build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        final DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
-        auth.setUserDetailsService(userService);    // set the custom user details
-        auth.setPasswordEncoder(passwordEncoder()); // set the password encoder - bcrypt
-        return auth;
     }
 
 }
